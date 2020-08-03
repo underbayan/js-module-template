2f16cc8c136f1f365a3128a2d90a780bb1ee8c6e
dairuomeng <dairuomeng@theduapp.com>
2020-08-03 10:07:02 +0800

    feat: remove useless files

--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-const lint = ['prettier --write', 'eslint src --fix', 'git add'];
-const onlyPrettier = ['prettier --write', 'git add'];
-module.exports = {
-	'src/**/*.js': lint,
-	'test/**/*.js': onlyPrettier,
-};
